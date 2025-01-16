7dfca87fd7dc5c56db1e2a0f65a3a531e4f467f0
sehyeogi365 <lsh27572757@gmail.com>
2025-01-16 16:52:06 +0900

    feat: ErrorCode Enum 추가메시지

--- src/main/java/kr/hhplus/be/server/domain/error/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/domain/error/ErrorCode.java
@@ -13,7 +13,11 @@ public enum ErrorCode {
     NOT_SUPPORTED_HTTP_METHOD(HttpStatus.BAD_REQUEST,"지원하지 않는 Http Method 방식입니다."),
     NOT_VALID_METHOD_ARGUMENT(HttpStatus.BAD_REQUEST,"유효하지 않은 Request Body 혹은 Argument입니다."),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 사용자를 찾을 수 없습니다."),
-    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 상품을 찾을 수 없습니다.");
+    POINT_NOT_FOUND(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
+    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 상품을 찾을 수 없습니다."),
+    COUPON_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 쿠폰을 찾을 수 없습니다."),
+    ORDER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 주문내역을 찾을 수 없습니다."),
+    PAYMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 결제내역을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
