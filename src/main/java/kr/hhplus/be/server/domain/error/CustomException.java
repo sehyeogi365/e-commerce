bcf7b0e3c1c916af3f283d4d40a98cbd22443706
sehyeogi365 <lsh27572757@gmail.com>
2025-01-16 10:42:15 +0900

    feat: ApiControllerAdvice 오류 안뜨게 수정, CustomException에 ErroCode Enum 추가

--- src/main/java/kr/hhplus/be/server/domain/error/CustomException.java
+++ src/main/java/kr/hhplus/be/server/domain/error/CustomException.java
@@ -10,4 +10,10 @@ public class CustomException extends RuntimeException {
     private HttpStatus httpStatus;
     private String message;
     private ErrorCode errorCode;
+    public CustomException(ErrorCode errorCode) {//에러코드 필수로 받아오기
+        super(errorCode.getMessage());
+        this.httpStatus = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+        this.errorCode = errorCode;
+    }
 }
--- src/main/java/kr/hhplus/be/server/interfaces/ApiControllerAdvice.java
+++ src/main/java/kr/hhplus/be/server/interfaces/ApiControllerAdvice.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.interfaces;
 
 
 
+import kr.hhplus.be.server.domain.error.CustomException;
 import kr.hhplus.be.server.domain.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +15,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        logger.error("Unhandled exception occurred: ", e);
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
+        logger.error("Unhandled exception occurred: ", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("400", e.getMessage()));
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        logger.error("Unhandled exception occurred: ", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("500", "서버 내부 오류가 발생했습니다."));
     }
 
