a5ab0025ac6910a1c8a069b55273cee4ae1ce36b
sehyeogi365 <lsh27572757@gmail.com>
2025-02-10 16:18:58 +0900

    feat: 포인트 컨트롤러 테스트

--- src/test/java/kr/hhplus/be/server/interfaces/point/controller/PointControllerTest.java
+++ src/test/java/kr/hhplus/be/server/interfaces/point/controller/PointControllerTest.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.interfaces.point.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.hhplus.be.server.domain.point.service.PointService;
 import kr.hhplus.be.server.interfaces.point.dto.PointRequest;
+import kr.hhplus.be.server.interfaces.point.dto.PointResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +14,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import static org.hibernate.Hibernate.get;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -40,6 +42,29 @@ class PointControllerTest {
 
 //    1. 일단 레디스 사용한 곳 임시로 주석처리하기
 //2. 레디스까지 통째로 세팅해서 다시 테스트 해보기
+
+    @Test
+    @DisplayName("포인트 잔액조회")
+    void 포인트_잔액조회() throws Exception {
+        // given
+        long userId = 1L;
+        int point = 10000;
+
+        PointResponse pointResponse = new PointResponse(userId, point);
+
+        when(pointService.getUserPoint(userId)).thenReturn(pointResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/point/{userId}", userId)
+                        .content(objectMapper.writeValueAsString(pointResponse))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.userId").value(1))
+                .andExpect(jsonPath("$.data.point").value(10000));
+    }
+
+
+
     @Test
     @DisplayName("포인트 충전")
     void 포인트_충전() throws Exception {
@@ -76,11 +101,9 @@ class PointControllerTest {
         // when&then
         mockMvc.perform(post("/api/v1/point/charge")
                         .content(objectMapper.writeValueAsString(pointRequest))
-                        //.content(validRequest)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                 ).andExpect(status().isOk())
-//                .andExpect(jsonPath("$.success").value(true));
                 .andExpect(jsonPath("$.data.userId").value(1))
                 .andExpect(jsonPath("$.data.point").value(10001));
     }
