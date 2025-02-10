6b556ceba8b173eac0e2925ee54f84eb12aee123
sehyeogi365 <lsh27572757@gmail.com>
2025-02-10 11:36:36 +0900

    feat: 포인트 컨트롤러 테스트 실패케이스 성공

--- src/test/java/kr/hhplus/be/server/interfaces/point/controller/PointControllerTest.java
+++ src/test/java/kr/hhplus/be/server/interfaces/point/controller/PointControllerTest.java
@@ -1,20 +1,28 @@
 package kr.hhplus.be.server.interfaces.point.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hhplus.be.server.domain.point.service.PointService;
 import kr.hhplus.be.server.interfaces.point.dto.PointRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.http.MediaType;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 
-
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @SpringBootTest
@@ -27,18 +35,22 @@ class PointControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockitoBean
+    private PointService pointService;
+
 //    1. 일단 레디스 사용한 곳 임시로 주석처리하기
 //2. 레디스까지 통째로 세팅해서 다시 테스트 해보기
     @Test
     @DisplayName("포인트 충전")
     void 포인트_충전() throws Exception {
-        //given
+        // given
         long userId = 1L;
-        int point = 10000;
+        int point = 100000;
 
         PointRequest pointRequest = new PointRequest(userId, point);
 
-        //when&then
+        when(pointService.chargePoint(userId,point)).thenReturn(pointRequest.getPoint());
+        // when&then
         mockMvc.perform(post("/api/v1/point/charge")
                         .content(objectMapper.writeValueAsString(pointRequest))
                         //.content(validRequest)
@@ -47,25 +59,21 @@ class PointControllerTest {
                 .andExpect(status().isOk())
 //                .andExpect(jsonPath("$.success").value(true));
                 .andExpect(jsonPath("$.data.userId").value(1))
-                .andExpect(jsonPath("$.data.point").value(10000));
+                .andExpect(jsonPath("$.data.point").value(100000));
     }
 
     @Test
     @DisplayName("포인트 충전: 10000단위가 아닐시")
     void 포인트_충전2() throws Exception {
-        //given
-//        String validRequest = """
-//            {
-//                "userId": 1,
-//                "point": 10001
-//            }
-//        """;
+        // given
         long userId = 1L;
-        int point = 10000;
+        int point = 10001;
 
         PointRequest pointRequest = new PointRequest(userId, point);
 
-        //when&then
+        when(pointService.chargePoint(userId,point)).thenReturn(pointRequest.getPoint());
+
+        // when&then
         mockMvc.perform(post("/api/v1/point/charge")
                         .content(objectMapper.writeValueAsString(pointRequest))
                         //.content(validRequest)
