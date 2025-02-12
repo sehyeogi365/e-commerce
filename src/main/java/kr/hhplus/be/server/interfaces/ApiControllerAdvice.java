98c801038a9bea8e5d9e189c108420cd755f9c4a
sehyeogi365 <lsh27572757@gmail.com>
2025-02-12 15:26:07 +0900

    feat: 일부 커스텀 익셉션 핸들러 수정

--- src/main/java/kr/hhplus/be/server/interfaces/ApiControllerAdvice.java
+++ src/main/java/kr/hhplus/be/server/interfaces/ApiControllerAdvice.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.interfaces;
 
 
 import kr.hhplus.be.server.domain.error.CustomException;
+import kr.hhplus.be.server.domain.error.ErrorCode;
 import kr.hhplus.be.server.domain.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +26,18 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("400", e.getMessage()));
     }
 
+//    @ExceptionHandler(CustomException.class)
+//    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+//        logger.error("Unhandled exception occurred: ", e);
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("500", "서버 내부 오류가 발생했습니다."));
+//    }
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        // CustomException에서 ErrorCode를 가져와 상태 코드와 메시지를 처리
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus().toString(), errorCode.getMessage());
         logger.error("Unhandled exception occurred: ", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("500", "서버 내부 오류가 발생했습니다."));
+        // 상태 코드와 메시지에 맞는 응답 반환
+        return ResponseEntity.status(errorCode.getStatus()).body(errorResponse);
     }
-
 }
