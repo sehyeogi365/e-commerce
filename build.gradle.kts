104fe1c646c8eadb784a7b2e77e872786478b812
sehyeogi365 <lsh27572757@gmail.com>
2025-02-24 10:27:50 +0900

    feat: 카프카 안되는 이유 이제 도커 컨테이너에서 세개 브로커가 돌아가는 거 확인 되나 무한로딩 현상은 계속된다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
 	// kafka
 	implementation ("org.springframework.kafka:spring-kafka")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 }
 
 tasks.withType<Test> {
